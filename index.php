ae44a66c3895e8e8b1ea5192f44c77a2e3254817
christinagyles_ea <christina.gyles@environment-agency.gov.uk>
2020-06-28 08:00:33 +0100

    updating files to php

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 <?php include_once("index.html"); ?>
+<!DOCTYPE html>
+    <html>
+
+        <head>
+            <title>Christina'a Art Club entries</title>
+            <link href="style.css" rel="stylesheet">
+
+        </head>
+
+        <body>
+            <h1 style="font-family: Arial, Helvetica, sans-serif;">Christina's art</h1>
+            <a style="font-family: Arial, Helvetica, sans-serif;" href="/loading.html">Cityscape</a></p>
+            <?php
+            // This is a single line comment
+            # This is also a single line comment
+            echo "Hello, world!";
+            ?>
+            <h1><?php echo "Hello, world!"; ?></h1>
+        </body>
+
+    </html>
