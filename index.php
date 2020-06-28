c06bcfc60e55c435235c458375791ee47152a472
christinagyles_ea <christina.gyles@environment-agency.gov.uk>
2020-06-28 08:49:25 +0100

    trying to define a constant

--- index.php
+++ index.php
@@ -13,14 +13,19 @@
         // Declaring variables
 $txt = "See you on the other side!";
 $number = 101;
+// Defining constant
+define("SITE_URL", "https://www.tutorialrepublic.com/");
 ?>
             <h1 style="font-family: Arial, Helvetica, sans-serif;">Christina's art</h1>
             <a style="font-family: Arial, Helvetica, sans-serif;" href="/loading.php">Cityscape</a></p>
+
+
  <?php
      // This is art
             # This is also art
 
-
+// Using constant
+echo 'Thank you for visiting - ' . SITE_URL;
  
 // Displaying variables value
 echo $txt;  // See you on the other side!
